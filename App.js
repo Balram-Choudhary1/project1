89143b1b32c499bdb0f449889d62e7fe4ea0c039
Balram-Choudhary1 <balramchoudhary1608@gmail.com>
2023-12-27 00:37:54 +0530

    no changes

--- App.js
+++ App.js
@@ -10,7 +10,7 @@
 // export default App
 
 
-// //Dark theme
+//Dark theme
 // import { View, Text, Switch,StatusBar } from 'react-native'
 // import React from 'react'
 // import { useColorScheme } from 'nativewind'
@@ -36,66 +36,70 @@
 
 
 // Import necessary modules
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
+// import React, { useState } from 'react';
+// import { View, Text, TouchableOpacity } from 'react-native';
+// import RNPickerSelect from 'react-native-picker-select';
 
-// Define the component
-const MonthDropdown = () => {
-  // State to store the selected month
-  const [selectedMonth, setSelectedMonth] = useState(null);
+// // Define the component
+// const MonthDropdown = () => {
+//   // State to store the selected month
+//   const [selectedMonth, setSelectedMonth] = useState(null);
 
-  // Array of 12 months
-  const months = [
-    { label: 'January', value: 'January' },
-    { label: 'February', value: 'February' },
-    { label: 'March', value: 'March' },
-    { label: 'April', value: 'April' },
-    { label: 'May', value: 'May' },
-    { label: 'June', value: 'June' },
-    { label: 'July', value: 'July' },
-    { label: 'August', value: 'August' },
-    { label: 'September', value: 'September' },
-    { label: 'October', value: 'October' },
-    { label: 'November', value: 'November' },
-    { label: 'December', value: 'December' },
-  ];
+//   // Array of 12 months
+//   const months = [
+//     { label: 'January', value: 'January' },
+//     { label: 'February', value: 'February' },
+//     { label: 'March', value: 'March' },
+//     { label: 'April', value: 'April' },
+//     { label: 'May', value: 'May' },
+//     { label: 'June', value: 'June' },
+//     { label: 'July', value: 'July' },
+//     { label: 'August', value: 'August' },
+//     { label: 'September', value: 'September' },
+//     { label: 'October', value: 'October' },
+//     { label: 'November', value: 'November' },
+//     { label: 'December', value: 'December' },
+//   ];
 
-  // Function to handle the submission of the selected value
-  const handleSubmit = () => {
-    if (selectedMonth) {
-      // Handle the selected month, you can perform any action here
-      console.log('Selected Month:', selectedMonth);
-    } else {
-      // No month selected
-      console.log('Please select a month');
-    }
-  };
+//   // Function to handle the submission of the selected value
+//   const handleSubmit = () => {
+//     if (selectedMonth) {
+//       // Handle the selected month, you can perform any action here
+//       console.log('Selected Month:', selectedMonth);
+//     } else {
+//       // No month selected
+//       console.log('Please select a month');
+//     }
+//   };
 
-  return (
-    <View style={{ padding: 20 }}>
-      {/* Dropdown component */}
-      <RNPickerSelect
-        placeholder={{ label: 'Select a month', value: null }}
-        items={months}
-        onValueChange={(value) => setSelectedMonth(value)}
-        value={selectedMonth}
-      />
+//   return (
+//     <View style={{ padding: 20 }}>
+//       {/* Dropdown component */}
+//       <RNPickerSelect
+//         placeholder={{ label: 'Select a month', value: null }}
+//         items={months}
+//         onValueChange={(value) => setSelectedMonth(value)}
+//         value={selectedMonth}
+//       />
 
-      {/* Select button */}
-      <TouchableOpacity
-        style={{
-          backgroundColor: '#007BFF',
-          padding: 10,
-          borderRadius: 5,
-          marginTop: 10,
-        }}
-        onPress={handleSubmit}
-      >
-        <Text style={{ color: 'white', textAlign: 'center' }}>Select</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+//       {/* Select button */}
+//       <TouchableOpacity
+//         style={{
+//           backgroundColor: '#007BFF',
+//           padding: 10,
+//           borderRadius: 5,
+//           marginTop: 10,
+//         }}
+//         onPress={handleSubmit}
+//       >
+//         <Text style={{ color: 'white', textAlign: 'center' }}>Select</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
 
-export default MonthDropdown;
+// export default MonthDropdown;
+
+
+
+
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
+
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ["nativewind/babel",'react-native-reanimated/plugin',
-   ],
+  env: {
+    production: {
+      plugins: ['react-native-paper/babel'],
+    },
+  },
 };
-
